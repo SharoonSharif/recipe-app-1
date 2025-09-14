7feeb44b7a6967e50498bd54ad8a7dba51465da5
Sharoon Sharif <150296639+SharoonSharif@users.noreply.github.com>
2025-09-13 22:56:09 -0500

    Remove ConvexProvider from root route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,10 @@
 // src/routes/__root.tsx
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { ConvexProvider, ConvexReactClient } from 'convex/react'
-
-const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL)
 
 export const Route = createRootRoute({
   component: () => (
-    <ConvexProvider client={convex}>
-      <div className="min-h-screen">
-        <Outlet />
-      </div>
-    </ConvexProvider>
+    <div className="min-h-screen">
+      <Outlet />
+    </div>
   ),
 })
